36578ab300e3d2cd091fed6b48afd434206c25ac
KelisonMeneses <154636454+KelisonMeneses@users.noreply.github.com>
2025-03-08 23:40:17 +0000

    Deu certo

--- buscadorDDD.rb
+++ buscadorDDD.rb
@@ -1,8 +1,21 @@
 require 'faraday'
 require 'json'
 
-puts "Digite um DDD:"
+puts 'Digite um DDD:'
 ddd = gets.chomp
 
-resposta = Faraday.get(https://brasilapi.com.br/api/ddd/v1/{ddd})
+resposta = Faraday.get("https://brasilapi.com.br/api/ddd/v1/#{ddd}")
 corpo = JSON.parse(resposta.body)
+puts "Status da resposta: #{resposta.status}"
+
+case resposta.status
+
+when 200
+    puts "Estado: #{corpo['state']}"
+
+when 404
+    puts "DDD invalido."
+when 500..599
+    puts "Erro na API, tente novamente mais tarde."
+
+end
